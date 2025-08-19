142e795da56b021c2b93cf46866efa16d29967fc
Megan Zheng <megan_zheng@brown.edu>
2025-08-19 09:00:14 -0500

    commit extension

---
+++ jupyter_fs/handlers.py
@@ -0,0 +1,47 @@
+import json
+import subprocess, os
+
+from jupyter_server.base.handlers import APIHandler
+from jupyter_server.utils import url_path_join
+import tornado
+
+class RouteHandler(APIHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.finish(json.dumps({
+            "data": "This is /jupyter-fs/get-example endpoint!"
+        }))
+        
+class RouteHandlerCommit(APIHandler):
+    @tornado.web.authenticated
+    def get(self):
+        script_path = "./jupyter_fs/script/commit.sh"
+        cur_dir = os.getcwd()
+        result = subprocess.run(["bash", script_path], capture_output=True, text=True)
+        self.write(json.dumps({
+            "curdir": cur_dir, 
+            "hash": result.stdout, 
+            "error": result.stderr
+        }))
+
+class RouteHandlerRevert(APIHandler):
+    @tornado.web.authenticated
+    def get(self):
+        script_path = ""
+        hash = self.get_argument("hash")
+        self.write(json.dumps({
+            "curdir": "reverting", 
+            "hash": hash, 
+            "error": ""
+        }))
+        
+
+def setup_handlers(web_app):
+    host_pattern = ".*$"
+
+    base_url = web_app.settings["base_url"]
+    route_pattern = url_path_join(base_url, "jupyter-fs", "get-example")
+    route_pattern_commit = url_path_join(base_url, "jupyter-fs", "make-commit")
+    route_pattern_revert = url_path_join(base_url, "jupyter-fs", "make-revert")
+    handlers = [(route_pattern, RouteHandler), (route_pattern_commit, RouteHandlerCommit), (route_pattern_revert, RouteHandlerRevert)]
+    web_app.add_handlers(host_pattern, handlers)
---
+++ src/index.ts
@@ -0,0 +1,118 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import { requestAPI } from './handler';
+import { ICommandPalette } from '@jupyterlab/apputils';
+import { Notebook, NotebookActions, NotebookPanel } from '@jupyterlab/notebook';
+import { ICodeCellModel, ICellModel, Cell, CodeCell } from '@jupyterlab/cells';
+
+/**
+ * Initialization data for the jupyter-fs extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyter-fs:plugin',
+  description: 'A JupyterLab extension (fs aware).',
+  autoStart: true,
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd) => {
+    const { commands } = app;
+    const command = 'Dependencies:get-fs';
+
+    console.log('JupyterLab extension jupyter-fs is activated!');
+
+    let cellToCommit = new Map<string, string>();
+    let listenerAttached = false;
+
+    async function makeCommit(
+      c: Cell<ICodeCellModel>,
+      cid: string,
+      panel: NotebookPanel
+    ) {
+      try {
+        // Make commit and get commit hash.
+        const data = await requestAPI<any>('make-commit');
+        console.log('data.hash: ', data);
+        if (data.error) {
+          console.log('data error: ', data.error);
+        }
+        cellToCommit.set(cid, data.hash);
+
+        // Wait for cell to run.
+        const notebook = panel.content as Notebook;
+        await NotebookActions.runCells(notebook, [c], panel.sessionContext);
+        console.log('finished running cell!');
+      } catch (reason) {
+        console.error(
+          `The jupyter_fs server extension appears to be missing.\n${reason}`
+        );
+      }
+    }
+
+    async function setup(panel: NotebookPanel) {
+      let cells = panel.content.widgets as Cell<ICellModel>[];
+      if (cells && cells.length > 0) {
+        for (const cell of cells) {
+          if (cell && cell.model.type === 'code') {
+            const codeCell = cell as Cell<ICodeCellModel>;
+            const cid = codeCell.model.id;
+            console.log('Running cell: ', cid);
+            await makeCommit(codeCell, cid, panel);
+          }
+        }
+      }
+      console.log('hash map after setup: ', cellToCommit);
+    }
+
+    async function revert(hash: string) {
+      console.log('reverting to hash: ', hash);
+      const data = await requestAPI<any>(`revert-commit?hash=${hash}`);
+      console.log('data: ', data);
+    }
+
+    // Listen to when a cell is scheduled for execution; revert the state
+    // of the FS when a cell is about to execute.
+    let onScheduled = (nb: any, data: any) => {
+      const c = data.cell;
+      if (c && c instanceof CodeCell) {
+        const cid = c.model.id;
+        const hash = cellToCommit.get(cid);
+        if (hash) {
+          console.log(`cid's commit: ${cid}, ${hash}`);
+          revert(hash);
+        } else {
+          throw new Error(`cannot find commit for code cell: ${cid}`);
+        }
+      }
+    };
+
+    commands.addCommand(command, {
+      label: 'FS',
+      caption: 'Reacting to FS changes.',
+      execute: (args: any) => {
+        console.log('begin fs extension');
+        if (app && app.shell && app.shell.currentWidget) {
+          const panel = app.shell.currentWidget as NotebookPanel;
+          setup(panel);
+
+          // Attach execution listener after set-up.
+          if (!listenerAttached) {
+            NotebookActions.executionScheduled.connect(onScheduled);
+          }
+        }
+      }
+    });
+
+    requestAPI<any>('get-example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The jupyter_fs server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
